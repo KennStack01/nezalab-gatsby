2afa6df0831e80841d6476090144760813c60b64
Kenn Stack <kibadikennedy@gmail.com>
2021-09-27 15:51:56 +0800

    modified the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import abductionIllustration from "../images/abduction-illustration.svg";
@@ -14,9 +14,16 @@ function NotFoundPage() {
           className="block mx-auto w-1/2"
           src={abductionIllustration}
         />
-        <h2 className="bg-yellow-400 text-2xl font-bold inline-block my-8 p-3">
+        <h2 className="bg-gray-700 md:text-2xl text-xl text-center mx-auto font-bold inline-block my-8 p-3">
           Looks like this page is a ghost that got abducted by aliens...
         </h2>
+
+        <Link
+          to="/"
+          className="bg-jaffa-600 p-3 text-center mx-auto my-5 text-xl font-semibold "
+        >
+          Go to Home Page →
+        </Link>
       </div>
     </Layout>
   );
